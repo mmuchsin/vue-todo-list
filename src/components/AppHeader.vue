edc40e4faec145854b90593db495affe5886f7bb
mmuchsin <kangucin21@gmail.com>
2024-02-17 07:40:19 +0700

    feat(components): :art: add AppHeader, TodoList, and ListItem components
    - Create AppHeader component for the main application header. - Implement TodoList component to display a list of todo items. - Add ListItem component to represent individual todo items.

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <h1><span class="icon" aria-hidden="true">✅</span> To do</h1>
+    <p>Building Real-world Web Applications with Vue.js 3</p>
+  </header>
+</template>
+
+<style scoped>
+header {
+  border-bottom: #fff 1px solid;
+  background-color: #333;
+}
+
+header::after {
+  content: "";
+  display: block;
+  height: 1px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+}
+
+h1 {
+  font-size: 2rem;
+}
+
+h1 .icon {
+  font-size: 1rem;
+  vertical-align: middle;
+}</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import ListItem from './ListItem.vue'
+</script>
+
+<template>
+    <ul>
+        <ListItem :is-checked="false">This is the slotted content</ListItem>
+    </ul>
+</template>
