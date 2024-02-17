f5b960025aed886022ca036dc9fe6a7240fb4592
mmuchsin <kangucin21@gmail.com>
2024-02-17 08:13:05 +0700

    feat(TodoList): :sparkles: add strikethrough effect on checkmark
    Implement visual feedback for completed tasks by adding a strikethrough style to todo list items when they are marked as checked.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,56 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
+import type { Ref } from 'vue'
 import ListItem from './ListItem.vue'
+
+type Item = {
+    title: string,
+    checked?: boolean
+}
+
+const listItems: Ref<Item[]> = ref([
+    { title: 'Make a todo list app', checked: true },
+    { title: 'Predict the weather', checked: false },
+    { title: 'Play some tunes', checked: false },
+    { title: 'Let\'s get cooking', checked: false },
+    { title: 'Pump some iron', checked: false },
+    { title: 'Track my expenses', checked: false },
+    { title: 'Organize a game night', checked: false },
+    { title: 'Learn a new language', checked: false },
+    { title: 'Publish my work' }
+])
+
+const updateItem = (item: Item): void => {
+    const updatedItem = findItemInList(item)
+    if (updatedItem) {
+        toggleItemChecked(updatedItem)
+    }
+}
+
+const findItemInList = (item: Item): Item | undefined => {
+    return listItems.value.find(
+        (itemInList: Item) => itemInList.title === item.title
+    )
+}
+const toggleItemChecked = (item: Item): void => {
+    item.checked = !item.checked
+}
 </script>
 
 <template>
     <ul>
-        <ListItem :is-checked="false">This is the slotted content</ListItem>
+        <li :key='key' v-for='(item, key) in listItems'>
+            <ListItem :is-checked='item.checked' @:click.prevent="updateItem(item)">{{ item.title }}</ListItem>
+        </li>
     </ul>
 </template>
+
+<style scoped>
+ul {
+    list-style: none;
+}
+
+li {
+    margin: 0.4rem 0;
+}
+</style>
