22c66b0bf24049f4ee8babcb7e245abfb0990a41
mmuchsin <kangucin21@gmail.com>
2024-02-17 11:29:04 +0700

    feat(TodoList): :sparkles: implement local storage for todo items
    Add functions to set and get todo list items from local storage. Initialize the todo list with default items if local storage is empty on component mount. This ensures that user's todo items are persisted across sessions.  The `setToStorage` function serializes the todo items array and stores it in local storage under the key 'list-items'. The `getFromStorage` function retrieves and parses the serialized todo items array from local storage, providing an empty array if no data is found.  The `initListItems` function checks if the local storage is empty and, if so, initializes it with a predefined list of todo items. This list is then saved to local storage and used to set the `storageItems` reactive property.  Finally, the `onMounted` lifecycle hook calls `initListItems` and updates `storageItems` with the data retrieved from local storage, ensuring that the component starts with the correct state.  These changes improve the user experience by maintaining the state of the todo list between sessions and providing a starting point for new users.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,61 +1,96 @@
 <script lang="ts" setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import type { Ref } from 'vue'
 import ListItem from './ListItem.vue'
 
 type Item = {
-    title: string,
-    checked?: boolean
+  title: string
+  checked?: boolean
 }
 
+const storageItems: Ref<Item[]> = ref([])
+
 const listItems: Ref<Item[]> = ref([
-    { title: 'Make a todo list app', checked: true },
-    { title: 'Predict the weather', checked: false },
-    { title: 'Play some tunes', checked: false },
-    { title: 'Let\'s get cooking', checked: false },
-    { title: 'Pump some iron', checked: false },
-    { title: 'Track my expenses', checked: false },
-    { title: 'Organize a game night', checked: false },
-    { title: 'Learn a new language', checked: false },
-    { title: 'Publish my work' }
+  { title: 'Make a todo list app', checked: true },
+  { title: 'Predict the weather', checked: false },
+  { title: 'Play some tunes', checked: false },
+  { title: "Let's get cooking", checked: false },
+  { title: 'Pump some iron', checked: false },
+  { title: 'Track my expenses', checked: false },
+  { title: 'Organize a game night', checked: false },
+  { title: 'Learn a new language', checked: false },
+  { title: 'Publish my work' }
 ])
 
 const updateItem = (item: Item): void => {
-    const updatedItem = findItemInList(item)
-    if (updatedItem) {
-        toggleItemChecked(updatedItem)
-    }
+  const updatedItem = findItemInList(item)
+  if (updatedItem) {
+    toggleItemChecked(updatedItem)
+    setToStorage(storageItems.value)
+  }
 }
-
 const findItemInList = (item: Item): Item | undefined => {
-    return listItems.value.find(
-        (itemInList: Item) => itemInList.title === item.title
-    )
+  return storageItems.value.find((itemInList: Item) => itemInList.title === item.title)
 }
 const toggleItemChecked = (item: Item): void => {
-    item.checked = !item.checked
+  item.checked = !item.checked
 }
 
 const sortedList = computed(() =>
-    [...listItems.value].sort((a, b) => (a.checked ? 1 : 0) -
-        (b.checked ? 1 : 0))
+  [...storageItems.value].sort((a, b) => (a.checked ? 1 : 0) - (b.checked ? 1 : 0))
 )
+
+const setToStorage = (items: Item[]): void => {
+  localStorage.setItem('list-items', JSON.stringify(items))
+}
+const getFromStorage = (): Item[] | [] => {
+  const stored = localStorage.getItem('list-items')
+  if (stored) {
+    return JSON.parse(stored)
+  }
+  return []
+}
+
+const initListItems = (): void => {
+  if (storageItems.value?.length === 0) {
+    const listItems = [
+      { title: 'Make a todo list app', checked: true },
+      { title: 'Predict the weather', checked: false },
+      { title: 'Read some comics', checked: false },
+      { title: "Let's get cooking", checked: false },
+      { title: 'Pump some iron', checked: false },
+      { title: 'Track my expenses', checked: false },
+      { title: 'Organise a game night', checked: false },
+      { title: 'Learn a new language', checked: false },
+      { title: 'Publish my work' }
+    ]
+    setToStorage(listItems)
+    storageItems.value = listItems
+  }
+}
+
+onMounted(() => {
+  initListItems()
+  storageItems.value = getFromStorage()
+})
 </script>
 
 <template>
-    <ul>
-        <li :key='key' v-for='(item, key) in sortedList'>
-            <ListItem :is-checked='item.checked' @:click.prevent="updateItem(item)">{{ item.title }}</ListItem>
-        </li>
-    </ul>
+  <ul>
+    <li :key="key" v-for="(item, key) in sortedList">
+      <ListItem :is-checked="item.checked" @:click.prevent="updateItem(item)">{{
+        item.title
+      }}</ListItem>
+    </li>
+  </ul>
 </template>
 
 <style scoped>
 ul {
-    list-style: none;
+  list-style: none;
 }
 
 li {
-    margin: 0.4rem 0;
+  margin: 0.4rem 0;
 }
 </style>
